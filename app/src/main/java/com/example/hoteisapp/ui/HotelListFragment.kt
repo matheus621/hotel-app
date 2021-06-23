024550526d7f34a47fee0108f01e8929798481a2
Matheus Souza <matheus.souza>
2021-06-23 14:28:37 -0300

    Adicionando exclusão de hoteis

--- app/src/main/java/com/example/hoteisapp/ui/HotelListFragment.kt
+++ app/src/main/java/com/example/hoteisapp/ui/HotelListFragment.kt
@@ -1,77 +1,150 @@
 package com.example.hoteisapp.ui
 
-import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
 import androidx.fragment.app.ListFragment
-import com.example.hoteisapp.database.SQLiteRepository
+import com.example.hoteisapp.Adapter
+import com.example.hoteisapp.R
 import com.example.hoteisapp.model.Hotel
 import com.example.hoteisapp.presenter.HotelListPresenter
-import com.example.hoteisapp.repository.HotelRepository
-import com.example.hoteisapp.repository.MemoryRepository
 import com.example.hoteisapp.view.HotelListView
-import kotlinx.coroutines.withContext
+import com.google.android.material.snackbar.Snackbar
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 
-class HotelListFragment: ListFragment(), HotelListView {
+class HotelListFragment : ListFragment(), HotelListView, AdapterView.OnItemLongClickListener,
+    ActionMode.Callback {
+    private var actionMode: ActionMode? = null
     private val presenter: HotelListPresenter by inject { parametersOf(this) }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         presenter.searchHotels("")
+        listView.onItemLongClickListener = this
     }
 
     override fun showHotels(hotels: List<Hotel>) {
-        val adapter = com.example.hoteisapp.Adapter(requireContext(),hotels)
+        val adapter = Adapter(requireContext(),hotels)
         listAdapter = adapter
     }
 
     override fun showHotelDetails(hotel: Hotel) {
-        if (activity is OnHotelClickListener){
+        if (activity is OnHotelClickListener) {
             val listener = activity as OnHotelClickListener
             listener.onHotelClick(hotel)
         }
     }
 
     override fun showDeleteMode() {
-        TODO("Not yet implemented")
+        val appCompatActivity = (activity as AppCompatActivity)
+        actionMode = appCompatActivity.startSupportActionMode(this)
+        listView.onItemLongClickListener = null
+        listView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
     }
 
     override fun hideDeleteMode() {
-        TODO("Not yet implemented")
+        listView.onItemLongClickListener = this
+        for (i in 0 until listView.count) {
+            listView.setItemChecked(i, false)
+        }
+        listView.post {
+            actionMode?.finish()
+            listView.choiceMode = ListView.CHOICE_MODE_NONE
+        }
     }
 
     override fun showSelectedHotels(hotels: List<Hotel>) {
-        TODO("Not yet implemented")
+        listView.post {
+            for (i in 0 until listView.count) {
+                val hotel = listView.getItemAtPosition(i) as Hotel
+                if (hotels.find { it.id == hotel.id } != null) {
+                    listView.setItemChecked(i, true)
+                }
+            }
+        }
     }
 
     override fun updateSelectionCountText(count: Int) {
-        TODO("Not yet implemented")
+        actionMode?.title =
+            resources.getQuantityString(R.plurals.list_hotel_selected, count, count)
     }
 
     override fun showMessageHotelsDeleted(count: Int) {
-        TODO("Not yet implemented")
+        Snackbar.make(listView,
+            getString(R.string.message_hotels_deleted, count),
+            Snackbar.LENGTH_LONG)
+            .setAction(R.string.undo) {
+                presenter.undoDelete()
+            }
+            .show()
     }
 
 
     override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
         super.onListItemClick(l, v, position, id)
         val hotel = l?.getItemAtPosition(position) as Hotel
-        presenter.showHotelDetails(hotel)
+        presenter.selectHotel(hotel)
     }
 
-    interface OnHotelClickListener{
+    interface OnHotelClickListener {
         fun onHotelClick(hotel: Hotel)
     }
 
-    fun search(text: String){
+    fun search(text: String) {
         presenter.searchHotels(text)
     }
 
-    fun clearSearch(){
+    fun clearSearch() {
         presenter.searchHotels("")
     }
 
+    override fun onItemLongClick(
+        parent: AdapterView<*>?,
+        view: View?,
+        position: Int,
+        id: Long
+    ): Boolean {
+        val consumed = (actionMode == null)
+        if (consumed) {
+            val hotel = parent?.getItemAtPosition(position) as Hotel
+            presenter.showDeleteMode()
+            presenter.selectHotel(hotel)
+        }
+        return consumed
+    }
+
+
+    override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.action_delete) {
+            presenter.deleteSelected { hotels ->
+                if (activity is OnHotelDeleteListener) {
+                    (activity as OnHotelDeleteListener).onHotelsDeleted(hotels)
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        activity?.menuInflater?.inflate(R.menu.hotel_delete_list, menu)
+        return true
+    }
+
+    override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean = false
+
+    override fun onDestroyActionMode(mode: ActionMode?) {
+        actionMode = null
+        presenter.hideDeleteMode()
+    }
+
+    interface OnHotelDeleteListener {
+        fun onHotelsDeleted(hotels: List<Hotel>)
+    }
 }
